b12de192d31f50d4b7088e2ef39f99944e3c5986
Alex Grant <alex@localnerve.com>
2024-09-24 15:34:36 -0400

    debug: posttranspile

--- posttranspile.js
+++ posttranspile.js
@@ -2,24 +2,36 @@ import fs from 'node:fs/promises';
 import path from 'node:path';
 
 async function processFile (filename) {
+  console.log(`@@@ posttranspile:file ${filename}`);
+
   const newname = filename.replace(/\.js$/, '.cjs');
-  fs.rename(filename, newname);
-  const data = await fs.readFile(filename, 'utf8');
+  console.log(`@@@ posttranspile:file:newname ${newname}`);
+  await fs.rename(filename, newname);
+  console.log('@@@ posttranspile:file:postrename');
+  const data = await fs.readFile(newname, 'utf8');
+  console.log('@@@ posttranspile:file:postreadfile');
   const newData = data.replaceAll('.js")', '.cjs")');
+  console.log('@@@ posttranspile:file:postreplace');
   await fs.writeFile(newname, newData);
+  console.log('@@@ posttranspile:file:postwritefile');
 }
 
 async function processDirectory (dir) {
-  const files = await fs.readdir(dir);
-  for (const file of files) {
-    const name = `${dir}${path.sep}${file}`;
-    const stats = await fs.stat(name);
-    if (stats.isDirectory()) {
-      processDirectory(name);
-    } else {
-      processFile(name);
+  console.log(`@@@ posttranspile:directory ${dir}`);
+  try {
+    const files = await fs.readdir(dir);
+    for (const file of files) {
+      const name = `${dir}${path.sep}${file}`;
+      const stats = await fs.stat(name);
+      if (stats.isDirectory()) {
+        await processDirectory(name);
+      } else {
+        await processFile(name);
+      }
     }
-  }  
+  } catch (err) {
+    console.error('posttranspile error:\n\n', err);
+  }
 }
 
 await processDirectory('./cjs');
