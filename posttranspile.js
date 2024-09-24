2280b9e9e3846405bc94394a9c2bd978c103ea6c
Alex Grant <alex@localnerve.com>
2024-09-24 15:37:13 -0400

    fix: posttranspile

--- posttranspile.js
+++ posttranspile.js
@@ -2,22 +2,14 @@ import fs from 'node:fs/promises';
 import path from 'node:path';
 
 async function processFile (filename) {
-  console.log(`@@@ posttranspile:file ${filename}`);
-
   const newname = filename.replace(/\.js$/, '.cjs');
-  console.log(`@@@ posttranspile:file:newname ${newname}`);
   await fs.rename(filename, newname);
-  console.log('@@@ posttranspile:file:postrename');
   const data = await fs.readFile(newname, 'utf8');
-  console.log('@@@ posttranspile:file:postreadfile');
   const newData = data.replaceAll('.js")', '.cjs")');
-  console.log('@@@ posttranspile:file:postreplace');
   await fs.writeFile(newname, newData);
-  console.log('@@@ posttranspile:file:postwritefile');
 }
 
 async function processDirectory (dir) {
-  console.log(`@@@ posttranspile:directory ${dir}`);
   try {
     const files = await fs.readdir(dir);
     for (const file of files) {
